e6a4adbdefcb7f7ccbf98cc96a424db70c6abb24
bendpx <ben@perimeterx.com>
2015-12-06 18:43:48 +0200

    Initial commit. plugin is working

---
+++ lua/pxchallenge.lua
@@ -0,0 +1,26 @@
+----------------------------------------------
+-- PerimeterX(www.perimeterx.com) Nginx plugin
+-- Version 1.0.0
+-- Release date: 15.12.2015
+----------------------------------------------
+local json = require "lua-json"
+
+function sendTo_Perimeter()
+    local pxdata = {}
+    pxdata['met'] = ngx.req.get_method();
+    pxdata['headers'] = ngx.req.get_headers();
+    pxdata['px_app_id'] = ngx.ctx.px_app_id;
+    pxdata['pxtoken'] = ngx.ctx.pxtoken;
+    pxdata['pxidentifier'] = ngx.ctx.pxidentifier;
+    pxdata = json.encode(pxdata);
+    if not (ngx.ctx.px_apiServer == nil) then
+        local report = [[curl -X POST -H "Content-Type: application/json" -d ]] .. [[']] .. pxdata .. [[']] .. " " .. ngx.ctx.px_apiServer .. '/api/v1/nginxcollect' .. [[ & ]]
+        os.execute(report);
+    end
+end
+
+sendTo_Perimeter();
+
+-- Sending the javascript challenge back to the client - expect a cookie set
+ngx.header["Content-type"] = "text/html"
+ngx.say('<script>var str = "' .. ngx.ctx.pxidentifier .. '";var strx = "";for (var i = 0; i < str.length; i++) {    strx += str[i];    if ((i + 1) % 4 == 0) {        strx += Math.random().toString(36).substring(3, 4);    }};document.cookie = "_pxcook=" + strx;window.location.reload();</script>');
---
+++ lua/pxnginx.lua
@@ -0,0 +1,68 @@
+----------------------------------------------
+-- PerimeterX(www.perimeterx.com) Nginx plugin
+-- Version 1.0.0
+-- Release date: 15.12.2015
+----------------------------------------------
+
+-- ## Configuration Block ##
+local px_token = 'my_temporary_token';
+local px_appId = 'PX6002';
+local px_apiServer = 'https://pxcollector.a.pxi.pub';
+local cookie_lifetime = 600 -- cookie lifetime, value in seconds
+-- ## END - Configuration block ##
+
+-- Validating the user key came from px-cookie and match against the locally generated one
+function validate_pxIdentidier(identifier, px_cookie)
+    local re_pxcook = ''
+
+    -- return
+    if px_cookie == nil or #px_cookie == 0 then
+        return false;
+    end
+
+    -- reverse client string builder
+    for i = 1, #px_cookie do
+        if not (i % 5 == 0) then
+            local c = px_cookie:sub(i, i)
+            re_pxcook = re_pxcook .. c;
+        end
+    end
+
+    -- no need to continure and check if length doesnt match
+    if not (#re_pxcook == #identifier) then
+        return false;
+    end
+
+    -- extract sha key from identifier and cookie
+    re_pxcook = ngx.decode_base64(re_pxcook)
+    identifier = ngx.decode_base64(identifier)
+
+    -- separting the timestamp and key from the cookie value
+    local re_pxcook_time = re_pxcook:sub(#re_pxcook - 9, #re_pxcook)
+    local re_pxcook_key = re_pxcook:sub(1, #re_pxcook - 10)
+    local identifier_key = identifier:sub(1, #re_pxcook - 10)
+
+    -- validate time is still in range
+    if tonumber(re_pxcook_time) + cookie_lifetime < ngx.time() then
+        return false;
+    end
+
+    -- validate key and cookie key
+    if not (identifier_key == re_pxcook_key) then
+        return false;
+    end
+    return true;
+end
+
+-- initilize identifier and cookie to perform check
+ngx.ctx.pxidentifier = gen_pxIdentifier();
+ngx.ctx.px_app_id = px_appId;
+ngx.ctx.px_apiServer = px_apiServer;
+ngx.ctx.px_token = px_token;
+local pxcook = ngx.var.cookie__pxcook;
+
+if not validate_pxIdentidier(ngx.ctx.pxidentifier, pxcook) then
+    return 1;
+end
+
+return 0;
