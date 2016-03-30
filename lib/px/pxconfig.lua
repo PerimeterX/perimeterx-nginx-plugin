bb8be1cb6d45fc22beecffb167c134030ed9e2ea
bendpx <ben@perimeterx.com>
2016-03-30 14:33:11 +0300

    bug fixes and enhancments

--- lib/px/pxconfig.lua
+++ lib/px/pxconfig.lua
@@ -2,11 +2,11 @@ local _M = {}
 
 -- ##  Configuration Block ##
 _M.px_token = 'my_temporary_token'
-_M.px_appId = 'PXAPPID'
+_M.px_appId = 'PX3tHq532g'
 _M.px_server = 'collector.a.pxi.pub'
 _M.px_port = 443
 _M.ssl_enabled = true
-_M.cookie_lifetime = 1200 -- cookie lifetime, value in seconds
+_M.cookie_lifetime = 3600 -- cookie lifetime, value in seconds
 _M.px_debug = false
 _M.px_maxbuflen = 500
 -- -- ## END - Configuration block ##
--- lib/px/pxnginx.lua
+++ lib/px/pxnginx.lua
@@ -8,10 +8,15 @@ local pxClient = require "px.pxclient"
 local config = require "px.pxconfig"
 
 -- local functions
+local ngx_encode_base64 = ngx.encode_base64
+local ngx_decode_base64 = ngx.decode_base64
+local ngx_time = ngx.time
 local ngx_log = ngx.log
 local ngx_req_get_method = ngx.req.get_method
 local ngx_req_get_headers = ngx.req.get_headers
+local ngx_hmac_sha1 = ngx.hmac_sha1
 local ngx_INFO = ngx.INFO
+local ngx_ERROR = ngx.ERR
 local tostring = tostring
 local tonumber = tonumber
 local string_sub = string.sub
@@ -30,7 +35,7 @@ for i = 1, #wluas do
     if ngx.var.http_user_agent and wluas[i] then
         local k = string.find(ngx.var.http_user_agent, wluas[i])
         if k == 1 then
-            ngx.log(ngx_INFO, "Whitelisted: ua_full")
+            ngx.log(ngx_ERROR, "Whitelisted: ua_full")
             return 0
         end
     end
@@ -41,7 +46,7 @@ local wluaf = pxFilters.Whitelist['ua_full']
 -- reverse client string builder
 for i = 1, #wluaf do
     if ngx.var.http_user_agent and wluaf[i] and ngx.var.http_user_agent == wluaf[i] then
-        ngx.log(ngx_INFO, "Whitelisted: ua_sub")
+        ngx.log(ngx_ERROR, "Whitelisted: ua_sub")
         return 0
     end
 end
@@ -52,7 +57,7 @@ local wlips = pxFilters.Whitelist['ip_addresses']
 -- reverse client string builder
 for i = 1, #wlips do
     if ngx.var.remote_addr == wlips[i] then
-        ngx_log(ngx_INFO, "Whitelisted: ip_addresses")
+        ngx_log(ngx_ERROR, "Whitelisted: ip_addresses")
         return 0
     end
 end
@@ -61,7 +66,7 @@ local wlfuri = pxFilters.Whitelist['uri_full']
 -- reverse client string builder
 for i = 1, #wlfuri do
     if ngx.var.uri == wlfuri[i] then
-        ngx_log(ngx_INFO, "Whitelisted: uri_full")
+        ngx_log(ngx_ERROR, "Whitelisted: uri_full")
         return 0
     end
 end
@@ -76,19 +81,27 @@ for i = 1, #wluri do
 end
 
 
-ngx_log(ngx_INFO, "Passed whitelisting filter")
 -- Generate an encrypted user-unique key
-local function gen_pxIdentifier()
-    local sec_now_str = tostring(ngx.time())
-    local ip = ngx.var.remote_addr
-    local ua = ngx.var.http_user_agent or ""
-    local identifier = ngx.hmac_sha1(config.px_token, config.px_appId .. ip .. ua);
-    return ngx.encode_base64(identifier .. sec_now_str)
+function gen_pxIdentifier()
+    local sec_now_str = tostring(ngx_time());
+    local ip = '';
+    if ngx.var.remote_addr then
+        ip = ngx.var.remote_addr;
+    end
+
+    local ua = '';
+    if ngx.var.http_user_agent then
+        ua = ngx.var.http_user_agent;
+    end
+
+    local identifier = ngx_hmac_sha1(px_token, px_appId .. ip .. ua);
+    return ngx_encode_base64(identifier .. sec_now_str);
 end
 
 -- Validating the user key came from px-cookie and match against the locally generated one
 local function validate_pxIdentifier(identifier, px_cookie)
     local re_pxcook = ''
+    ngx_log(ngx_ERROR, "VALIDATE COOKIE")
 
     -- if no cookie we stop validation
     if px_cookie == nil or #px_cookie == 0 then
@@ -109,8 +122,8 @@ local function validate_pxIdentifier(identifier, px_cookie)
     end
 
     -- extract sha key from identifier and cookie
-    re_pxcook = ngx.decode_base64(re_pxcook)
-    identifier = ngx.decode_base64(identifier)
+    re_pxcook = ngx_decode_base64(re_pxcook)
+    identifier = ngx_decode_base64(identifier)
 
     -- separting the timestamp and key from the cookie value
     local re_pxcook_time = re_pxcook:sub(#re_pxcook - 9, #re_pxcook)
@@ -118,7 +131,7 @@ local function validate_pxIdentifier(identifier, px_cookie)
     local identifier_key = identifier:sub(1, #re_pxcook - 10)
 
     -- validate time is still in range
-    if tonumber(re_pxcook_time) + config.cookie_lifetime < ngx.time() then
+    if tonumber(re_pxcook_time) + config.cookie_lifetime < ngx_time() then
         return false
     end
 
@@ -140,7 +153,6 @@ ngx.ctx.pxidentifier = gen_pxIdentifier()
 
 
 local pxcook = ngx.var.cookie__pxcook
-
 if not validate_pxIdentifier(ngx.ctx.pxidentifier, pxcook) then
     return 1
 end
