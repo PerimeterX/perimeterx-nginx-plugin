c181b3658d6900b477647c11a8038cda3b92080f
bendpx <ben@perimeterx.com>
2016-03-30 09:36:02 +0300

    extended cookie lifetime

--- lib/px/pxconfig.lua
+++ lib/px/pxconfig.lua
@@ -6,7 +6,7 @@ _M.px_appId = 'PXAPPID'
 _M.px_server = 'collector.a.pxi.pub'
 _M.px_port = 443
 _M.ssl_enabled = true
-_M.cookie_lifetime = 600 -- cookie lifetime, value in seconds
+_M.cookie_lifetime = 1200 -- cookie lifetime, value in seconds
 _M.px_debug = false
 _M.px_maxbuflen = 500
 -- -- ## END - Configuration block ##
