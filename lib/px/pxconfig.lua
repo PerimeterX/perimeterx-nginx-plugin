d54318af81c647b8713a0bac3dfe65c4d38079f6
bendpx <ben@perimeterx.com>
2016-03-31 09:08:23 +0300

    changed ip_from class

--- lib/px/pxconfig.lua
+++ lib/px/pxconfig.lua
@@ -3,7 +3,7 @@ local _M = {}
 -- ##  Configuration Block ##
 _M.px_token = 'my_temporary_token'
 _M.px_appId = 'PX3tHq532g'
-_M.px_server = 'collector.a.pxi.pub'
+_M.px_server = 'collector.staging.pxi.pub'
 _M.px_port = 443
 _M.ssl_enabled = true
 _M.cookie_lifetime = 3600 -- cookie lifetime, value in seconds
