81d8b2e4e15f21a564c6c507fcd32dad884ba0f9
nitzanpx <nitzan@perimeterx.com>
2017-10-17 17:49:52 +0300

    Update pxconstants.lua

--- lib/px/utils/pxconstants.lua
+++ lib/px/utils/pxconstants.lua
@@ -5,11 +5,11 @@
 ----------------------------------------------
 
 local _M = {
-MODULE_VERSION = "NGINX Module v2.12.0",
-RISK_PATH = "/api/v2/risk",
-CAPTCHA_PATH = "/api/v2/risk/captcha",
-ACTIVITIES_PATH = "/api/v1/collector/s2s",
-REMOTE_CONFIGURATIONS_PATH = "/api/v1/enforcer"
+  MODULE_VERSION = "NGINX Module v2.12.0",
+  RISK_PATH = "/api/v2/risk",
+  CAPTCHA_PATH = "/api/v2/risk/captcha",
+  ACTIVITIES_PATH = "/api/v1/collector/s2s",
+  REMOTE_CONFIGURATIONS_PATH = "/api/v1/enforcer"
 }
 
 return _M
