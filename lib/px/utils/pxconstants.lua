d63bb52300bf1ae49266c7994b47c512bf12957e
Yaron Schwimmer <yaron@perimeterx.com>
2017-12-01 15:37:32 -0800

    bump version

--- lib/px/utils/pxconstants.lua
+++ lib/px/utils/pxconstants.lua
@@ -15,7 +15,7 @@ local function readonlytable(table)
 end
 
 local _M = readonlytable {
-    MODULE_VERSION = "NGINX Module v2.4.0",
+    MODULE_VERSION = "NGINX Module v2.4.1",
     RISK_PATH = "/api/v2/risk",
     CAPTCHA_PATH = "/api/v1/risk/captcha",
     ACTIVITIES_PATH = "/api/v1/collector/s2s",
