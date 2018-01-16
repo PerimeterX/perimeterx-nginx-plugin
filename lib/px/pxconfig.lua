7336e3a9eaf5c412205f28c72ac4eb4480a41987
nitzanpx <nitzan@perimeterx.com>
2018-01-16 10:25:23 +0200

    revert pxconfiguation

--- lib/px/pxconfig.lua
+++ lib/px/pxconfig.lua
@@ -9,14 +9,13 @@ _M.px_enabled = true
 -- ##  Configuration Block ##
 
 -- ## Required Parameters ##
-_M.px_appId = 'PXvRfnOj4y'
-_M.cookie_secret = 'jm6XgPXeArcrW41Bb1U2tBGW30zl08gsJy5m8avhg6dMugo6vIr9cnppRbj6rHHG'
---_M.auth_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsicmlza19zY29yZSIsInJlc3RfYXBpIl0sImlhdCI6MTUxNDIwNjc1NSwic3ViIjoiUFh2UmZuT2o0eSIsImp0aSI6IjI4MmJkZTliLThlOTktNGNkMy04ZDg4LTUzODdiMjllOThlYyJ9.O2V7oU2nU0cuEeNK1YrX23OO-PTAtMP0zYKOB8YYR1g'
-_M.auth_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZXMiOlsicmlza19zY29yZSIsInJlc3RfYXBpIl0sImlhdCI6MTQ5NjY2MzQ2Niwic3ViIjoiUFh2UmZuT2o0eSIsImp0aSI6IjgwYjY1MmQ2LTZkN2MtNGNkNy1hNTdhLTU4MWUxMGZjN2JlNiJ9.M3QMB7GFndSE6U8slsMmYEU6H2h_elzQtx7_3EoMSIU'
+_M.px_appId = 'PX_APP_ID'
+_M.cookie_secret = 'COOKIE_KEY'
+_M.auth_token = 'PX_AUTH_TOKEN'
 
 -- ## Blocking Parameters ##
 _M.blocking_score = 100
-_M.block_enabled = true
+_M.block_enabled = false
 _M.captcha_enabled = true
 
 -- ## Additional Configuration Parameters ##
@@ -30,6 +29,10 @@ _M.additional_activity_handler = nil
 _M.enabled_routes = {}
 _M.first_party_enabled = true
 
+-- ## API protection mode ##
+_M.api_protection_mode = false
+_M.api_protection_block_url = nil
+_M.api_protection_default_redirect_url = nil
 
 -- ## Blocking Page Parameters ##
 _M.custom_logo = nil
@@ -48,21 +51,19 @@ _M.redirect_on_custom_url = false
 
 
 -- ## Debug Parameters ##
-_M.px_debug = true
+_M.px_debug = false
 _M.s2s_timeout = 1000
 _M.client_timeout = 2000
 _M.cookie_encrypted = true
 _M.px_maxbuflen = 10
-_M.px_port = 8080
-_M.ssl_enabled = false 
+_M.px_port = 443
+_M.ssl_enabled = true
 _M.enable_server_calls = true
 _M.send_page_requested_activity = true
--- _M.base_url = string.format('sapi-%s.perimeterx.net', _M.px_appId)
-_M.base_url = 'localhost' 
--- _M.collector_host = string.format('collector-%s.perimeterx.net', _M.px_appId)
-_M.collector_host = '10.20.1.217'
+_M.base_url = string.format('sapi-%s.perimeterx.net', _M.px_appId)
+_M.collector_host = string.format('collector-%s.perimeterx.net', _M.px_appId)
 _M.client_host = "client.perimeterx.net"
-_M.collector_port_overide = 8080 
+_M.collector_port_overide = nil
 _M.client_port_overide = nil
 -- ## END - Configuration block ##
 
