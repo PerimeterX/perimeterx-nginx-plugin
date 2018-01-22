5bff71ad98ad1eb893ad7f1ba75ea1841e1be71a
nitzanpx <nitzan@perimeterx.com>
2018-01-22 16:17:21 +0200

    fixed missing versions

--- lib/px/utils/pxconstants.lua
+++ lib/px/utils/pxconstants.lua
@@ -3,7 +3,7 @@
 ----------------------------------------------
 
 local _M = {
-    MODULE_VERSION = "NGINX Module v3.2.0",
+    MODULE_VERSION = "NGINX Module v3.2.1",
     RISK_PATH = "/api/v2/risk",
     CAPTCHA_PATH = "/api/v2/risk/captcha",
     ACTIVITIES_PATH = "/api/v1/collector/s2s",
