9e0d31967bfea76325ed32ef9c9f9e2058244c4f
ilaif <ilai.fallach@perimeterx.com>
2019-04-18 18:21:31 +0300

    logging

--- lib/px/utils/pxtelemetry.lua
+++ lib/px/utils/pxtelemetry.lua
@@ -11,6 +11,7 @@ function M.telemetry_check_header(px_config, px_client, px_headers, px_logger)
     end
 
     header_value = ngx.decode_base64(header_value)
+    px_logger.debug('header_value' .. header_value)
     local split_header_value = string.split(header_value, ':')
     if #split_header_value ~= 2 then
         px_logger.debug('Malformed ' .. px_constants.ENFORCER_TELEMETRY_HEADER .. ' header: ' .. header_value)
@@ -19,6 +20,7 @@ function M.telemetry_check_header(px_config, px_client, px_headers, px_logger)
     local given_hmac = split_header_value[2]
     local hmac_raw = hmac('sha256', px_config.cookie_secret, timestamp)
     local generated_hmac = px_common_utils.to_hex(hmac_raw)
+    px_logger.debug('timestamp: ' .. timestamp .. ', given_hmac: ' .. given_hmac .. ', hmac_raw: ' .. hmac_raw .. ', generated_hmac: ' .. generated_hmac)
     if given_hmac == generated_hmac then
         px_logger.debug('Received command to send enforcer telemetry')
         local details = {}
