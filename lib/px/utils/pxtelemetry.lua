9c8bdf1e185ee2b5522ddfb7f613d91efa1f9047
ilaif <ilai.fallach@perimeterx.com>
2019-04-21 15:11:52 +0300

    [ADD] check for timestamp expiry

--- lib/px/utils/pxtelemetry.lua
+++ lib/px/utils/pxtelemetry.lua
@@ -20,13 +20,14 @@ function M.telemetry_check_header(px_config, px_client, px_headers, px_logger)
     local given_hmac = string.upper(split_header_value[2])
     local hmac_raw = hmac('sha256', px_config.cookie_secret, timestamp)
     local generated_hmac = px_common_utils.to_hex(hmac_raw)
-    if given_hmac == generated_hmac then
+    local timestamp_number = tonumber(timestamp)
+    if given_hmac == generated_hmac and timestamp_number ~= nil and timestamp_number >= ngx_time() * 1000 then
         local details = {}
 		details.px_config = px_common_utils.filter_config(px_config);
 		details.update_reason = 'command'
         px_client.send_enforcer_telmetry(details);
     else
-        px_logger.debug(px_constants.ENFORCER_TELEMETRY_HEADER .. ' hmac validation failed. original: ' .. given_hmac .. '. generated hmac: ' .. generated_hmac)
+        px_logger.debug(px_constants.ENFORCER_TELEMETRY_HEADER .. ' hmac validation failed or timestamp expired. original: ' .. given_hmac .. '. generated hmac: ' .. generated_hmac)
     end
 end
 
