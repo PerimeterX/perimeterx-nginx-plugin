2570542c6bceea39274151f5602efd1ba2102147
Ilai Fallach <ilai.fallach@perimeterx.com>
2019-04-22 20:12:37 +0300

    Dev bd 765 telemetry timestamp expiration (#201)
    * [ADD] check for timestamp expiry
    
    * fix nginx time
    
    * add more verbose error
    
    * fix
    
    * add return
    
    * [CHANGE] pr fixes
    
    * [ADD] pcall handling telemetry check header function
    
    * Update pxnginx.lua

--- lib/px/utils/pxtelemetry.lua
+++ lib/px/utils/pxtelemetry.lua
@@ -20,14 +20,23 @@ function M.telemetry_check_header(px_config, px_client, px_headers, px_logger)
     local given_hmac = string.upper(split_header_value[2])
     local hmac_raw = hmac('sha256', px_config.cookie_secret, timestamp)
     local generated_hmac = px_common_utils.to_hex(hmac_raw)
-    if given_hmac == generated_hmac then
-        local details = {}
-		details.px_config = px_common_utils.filter_config(px_config);
-		details.update_reason = 'command'
-        px_client.send_enforcer_telmetry(details);
-    else
+    local timestamp_number = tonumber(timestamp)
+    local current_unix_time_ms = ngx.time() * 1000
+
+    if given_hmac ~= generated_hmac then
         px_logger.debug(px_constants.ENFORCER_TELEMETRY_HEADER .. ' hmac validation failed. original: ' .. given_hmac .. '. generated hmac: ' .. generated_hmac)
+        return
     end
+
+    if timestamp_number == nil or timestamp_number < current_unix_time_ms then
+        px_logger.debug(px_constants.ENFORCER_TELEMETRY_HEADER .. ' timestamp expired: ' .. timestamp .. ' < ' .. tostring(current_unix_time_ms))
+        return
+    end
+    
+    local details = {}
+    details.px_config = px_common_utils.filter_config(px_config)
+    details.update_reason = 'command'
+    px_client.send_enforcer_telmetry(details)
 end
 
 return M
