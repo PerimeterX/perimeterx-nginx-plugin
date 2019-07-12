cc07df3de41a12142af9ecdf6f8cc45ed6a34a08
Johnny Tordgeman <johnny@perimeterx.com>
2019-07-12 18:40:09 +0300

    global to local variables

--- lib/px/utils/pxcookiev1.lua
+++ lib/px/utils/pxcookiev1.lua
@@ -47,7 +47,7 @@ function PXCookieV1:validate(data)
 end
 
 function PXCookieV1:process()
-    cookie = ngx.ctx.px_orig_cookie
+    local cookie = ngx.ctx.px_orig_cookie
     if not cookie then
         local no_cookie_message = "no_cookie"
         if ngx.ctx.pxhd then
